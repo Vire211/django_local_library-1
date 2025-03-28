912cba8fc123bbae8d1d932015f8010ac294515b
Vire <85140503+Vire211@users.noreply.github.com>
2025-03-28 15:41:54 -0400

    Got the Ninja tutorial to work

--- locallibrary/api.py
+++ locallibrary/api.py
@@ -1,3 +1,4 @@
+import uuid
 from ninja import NinjaAPI, Schema
 
 api = NinjaAPI()
@@ -29,6 +30,7 @@ from catalog.models import Author, Genre, Book, BookInstance, Language
 from django.shortcuts import get_object_or_404
 from typing import List
 from datetime import date
+from typing import Optional
 
 # CRUD classes for Genre
 class GenreIn(Schema):
@@ -42,15 +44,15 @@ class GenreOut(Schema):
 class AuthorIn(Schema):
     first_name: str
     last_name: str
-    date_of_birth: date = None
-    date_of_death: date = None
+    date_of_birth: Optional[date] = None
+    date_of_death: Optional[date] = None
 
 class AuthorOut(Schema):
     id: int
     first_name: str
     last_name: str
-    date_of_birth: date = None
-    date_of_death: date = None
+    date_of_birth: Optional[date] = None
+    date_of_death: Optional[date] = None
 
 # CRUD classes for Language
 class LanguageIn(Schema):
@@ -75,22 +77,31 @@ class BookOut(Schema):
     author: AuthorOut
     summary: str
     isbn: str
-    genre: GenreOut
+    genre: List[GenreOut]
     language: LanguageOut
 
 # CRUD classes for BookInstance
 class BookInstanceIn(Schema):
     book: BookOut
     imprint: str
-    due_back: date
+    due_back: Optional[date] = None
     status: str
 
 class BookInstanceOut(Schema):
-    id: int
     book: BookOut
     imprint: str
-    due_back: date
+    due_back: Optional[date] = None
     status: str
+
+# Make every method have authentication except read
+from ninja.security import HttpBearer
+
+class GlobalAuth(HttpBearer):
+    def authenticate(self, request, token):
+        if token == "supersecret":
+            return token
+
+api = NinjaAPI(auth=GlobalAuth())
 
 # Create
 @api.post("/genres")
@@ -113,51 +124,57 @@ def create_book(request, payload: BookIn):
     book = Book.objects.create(**payload.dict())
     return {"id": book.id}
 
+@api.post("/bookinstances")
+def create_bookInstance(request, payload: BookInstanceIn):
+    book = BookInstance.objects.create(**payload.dict())
+    return {"book": book.book}
+
 # Retrieve
-@api.get("/genres/{genre_id}", response=GenreOut)
+@api.get("/genres/{genre_id}", response=GenreOut, auth=None)
 def get_genre(request, genre_id: int):
     genre = get_object_or_404(Genre, id=genre_id)
     return genre
 
-@api.get("/genres", response=List[GenreOut])
+@api.get("/genres", response=List[GenreOut], auth=None)
 def list_genres(request):
     qs = Genre.objects.all()
     return qs
 
-@api.get("/authors/{author_id}", response=AuthorOut)
+@api.get("/authors/{author_id}", response=AuthorOut, auth=None)
 def get_author(request, author_id: int):
     author = get_object_or_404(Author, id=author_id)
     return author
 
-@api.get("/authors", response=List[AuthorOut])
+@api.get("/authors", response=List[AuthorOut], auth=None)
 def list_authors(request):
     qs = Author.objects.all()
     return qs
 
-@api.get("/languages/{language_id}", response=LanguageOut)
+@api.get("/languages/{language_id}", response=LanguageOut, auth=None)
 def get_language(request, language_id: int):
     language = get_object_or_404(Language, id=language_id)
     return language
 
-@api.get("/languages", response=List[LanguageOut])
+@api.get("/languages", response=List[LanguageOut], auth=None)
 def list_languages(request):
     qs = Language.objects.all()
     return qs
 
-@api.get("/books/{book_id}", response=BookOut)
+@api.get("/books/{book_id}", response=BookOut, auth=None)
 def get_book(request, book_id: int):
     book = get_object_or_404(Book, id=book_id)
     return book
 
-@api.get("/books", response=List[BookOut])
+@api.get("/books", response=List[BookOut], auth=None)
 def list_books(request):
     qs = Book.objects.select_related("author")
     return list(qs)
 
-@api.get("/bookinstances", response=List[BookInstanceOut])
-def get_bookinstancess(request):
-    queryset = BookInstance.objects.select_related("book")
-    return list(queryset)
+@api.get("/bookinstances/{book_name}", response=List[BookInstanceOut], auth=None)
+def get_bookinstance(request, book_name: str):
+    books = BookInstance.objects.all()
+    l = books.filter(book__title=book_name)
+    return list(l)
 
 # Update
 @api.put("/genres/{genre_id}")
@@ -184,6 +201,22 @@ def update_author(request, author_id: int, payload: AuthorIn):
     author.save()
     return {"success": True}
 
+@api.put("/books/{book_id}")
+def update_book(request, book_id: int, payload: BookIn):
+    book = get_object_or_404(Book, id=book_id)
+    for attr, value in payload.dict().items():
+        setattr(book, attr, value)
+    book.save()
+    return {"success": True}
+
+@api.put("/bookinstances/{book_id}")
+def update_bookInstance(request, book_id: str, payload: BookInstanceIn):
+    book = get_object_or_404(BookInstance, id=book_id)
+    for attr, value in payload.dict().items():
+        setattr(book, attr, value)
+    book.save()
+    return {"success": True}
+
 # Delete
 @api.delete("/genres/{genre_id}")
 def delete_genre(request, genre_id: int):
@@ -206,5 +239,11 @@ def delete_language(request, language_id: int):
 @api.delete("/books/{book_id}")
 def delete_book(request, book_id: int):
     book = get_object_or_404(Book, id=book_id)
+    book.delete()
+    return {"success": True}
+
+@api.delete("/books/{book_id}")
+def delete_bookInstance(request, book_id: str):
+    book = get_object_or_404(BookInstance, id=book_id)
     book.delete()
     return {"success": True}
